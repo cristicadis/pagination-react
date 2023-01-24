3c0541be8a97f36c29d630d483f40c04302844a4
Cristian Cadis <cristiancadis@Cristians-MacBook-Air.local>
2023-01-24 15:10:36 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+import { useFetch } from './useFetch'
+import Follower from './Follower'
+function App() {
+  const { loading, data } = useFetch()
+  const [page, setPage] = useState(1)
+  const [followers, setFollowers] = useState([])
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+    
+  }, [loading, page])
+ 
+  const handlePage = (index) => {
+    setPage(index);
+  }
+  const nextPage = () => {
+    setPage((oldPage)=>{
+      if(oldPage===data.length-1){
+        return 0;
+      }
+      return oldPage + 1
+    })
+  }
+  const prevPage = () => {
+    setPage((oldPage)=>{
+      let prevPage = oldPage - 1
+      if (prevPage < 0) {
+        prevPage = data.length - 1
+      }
+      return prevPage;
+    })
+  }
+
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {followers.map((follower)=>{
+            return <Follower key={follower.id} {...follower} />
+          })}
+        </div>
+        {!loading &&
+        <div className="btn-container">
+        <button className="prev-btn" onClick={prevPage}>prev</button>
+          {data.map((item, index)=>{
+            return (
+            <button 
+              key={index } 
+              className={`page-btn ${index === page ? 'active-btn' : null}`}
+              onClick={()=>handlePage(index)}>{index + 1}</button>)
+          })}
+          <button className="next-btn" onClick={nextPage}>next</button>
+        </div> }
+      </section>
+    </main>
+    )
+}
+
+export default App
